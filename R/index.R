ea7a632f52f5792050129f996366bed45c049aa2
net-1 <139687643+net-1@users.noreply.github.com>
2023-07-18 04:06:35 +0530

    Add files via upload

---
+++ R/index.R
@@ -0,0 +1,64 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+index <- function() {
+  ind <- "
+
+-------------------------------------------------------------------------------------------------
+
+Practical 1
+Configure IP SLA tracking and path control topology.
+
+-------------------------------------------------------------------------------------------------
+
+Practical 2
+Implementation of BGP using AS_path attribute.
+
+-------------------------------------------------------------------------------------------------
+
+Practical 3
+Configuring IBGP and EBGP sessions.
+
+-------------------------------------------------------------------------------------------------
+
+Practical 4
+Secure Management Plane.
+
+-------------------------------------------------------------------------------------------------
+
+Practical 5
+Configure and verify path control using PBR (Policy Based Routing)
+
+-------------------------------------------------------------------------------------------------
+
+Practical 6
+Demonstrate Inter Vlan Routing.
+
+-------------------------------------------------------------------------------------------------
+
+Practical 7
+Simulating MP LS environment.
+
+Practical 8 ( prog(\"7\") ) 7 and 8 are combined
+Simulating VRF (Virtual Routing and Forwarding).
+-------------------------------------------------------------------------------------------------
+
+
+
+
+    "
+
+  cat(ind)
+}
